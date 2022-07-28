9855523fe5c994f8b46618d0ab0cf733062e5a0f
Patrick Esser <pesser@huth.org>
2022-07-28 08:50:40 +0200

    added automation for Go stubs

---
+++ make.bat
@@ -0,0 +1,1 @@
+protoc --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ./order/*.proto
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
+
+sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+protoc --go_out=./golang --go_opt=paths=source_relative \
+    --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+    ./${SERVICE_NAME}/*.proto
+cd golang/${SERVICE_NAME}
+go mod init github.com/docseltsam/microservices/golang/${SERVICE_NAME} || true
+go mod tidy
+cd ../../
+git config --global user.email "pesser@huth.org"
+git config --global user.name "Patrick Esser"
+git add . && git commit -am "proto update" || true
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+    -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tag/golang/§{SERVICE_NAME}/${RELEASE_VERSION}
