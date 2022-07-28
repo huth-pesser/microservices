29dfa0f9e123891e2bed0b2a1a7560310d4fd8b6
Patrick Esser <pesser@huth.org>
2022-07-28 09:40:59 +0200

    optimized

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 SERVICE_NAME=$1
 RELEASE_VERSION=$2
+USER_NAME=$3
+USER_EMAIL=$4
+
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+git fetch --all && git checkout main
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 mkdir ./golang
 mkdir ./golang/${SERVICE_NAME}
 protoc --go_out=./golang --go_opt=paths=source_relative \
@@ -14,10 +21,10 @@ cd golang/${SERVICE_NAME}
 go mod init github.com/docseltsam/microservices/golang/${SERVICE_NAME} || true
 go mod tidy
 cd ../../
-git config --global user.email "pesser@huth.org"
-git config --global user.name "Patrick Esser"
+
 git add . && git commit -am "proto update" || true
 git push origin HEAD:main
+
 git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
-    -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tag/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+  -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}" 
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
