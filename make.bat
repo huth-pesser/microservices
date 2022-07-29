083a02fe40312e8d21eab8bd12042d6778318456
Patrick Esser <pesser@huth.org>
2022-07-29 12:21:16 +0200

    order added

--- make.bat
+++ make.bat
@@ -1,1 +1,2 @@
 protoc --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ./product/*.proto
+protoc --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ./order/*.proto
